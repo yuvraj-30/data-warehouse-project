0b4f91ab81b33bba9d263d6f5966cac304c2785c
Yuvraj Singh <44972990+yuvraj-30@users.noreply.github.com>
2026-01-11 18:46:01 +1300

    Update file paths in run_all.sql for project relocation

--- run/run_all.sql
+++ run/run_all.sql
@@ -3,16 +3,16 @@ PRINT 'Starting end-to-end warehouse build...';
 -- =========================
 -- 0) Create database objects
 -- =========================
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\00_init_database.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\01_ddl_bronze.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\02_proc_load_bronze.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\03_ddl_silver.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\04_proc_load_silver.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\05_ddl_gold.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\05_proc_load_gold.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\06_quality_checks_silver.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\07_quality_checks_gold.sql"
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\08_dq_report_gold.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\00_init_database.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\01_ddl_bronze.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\02_proc_load_bronze.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\03_ddl_silver.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\04_proc_load_silver.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\05_ddl_gold.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\05_proc_load_gold.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\06_quality_checks_silver.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\07_quality_checks_gold.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\08_dq_report_gold.sql"
 
 -- =========================
 -- 1) Execute the pipeline
@@ -25,7 +25,7 @@ GO
 
 -- 1A) Load Bronze (CSV ingestion)
 EXEC bronze.proc_load_bronze
-    @bronze_folder_path = N'C:\DW\SQL_Warehouse_Project_Fortis_Aligned\data\bronze\';
+    @bronze_folder_path = N'C:\DW\SQL_Warehouse_Project\data\bronze\';
 
 -- 1B) Load Silver (clean + standardize)
 EXEC silver.proc_load_silver;
@@ -34,7 +34,7 @@ EXEC silver.proc_load_silver;
 EXEC gold.proc_load_gold;
 
 -- 1D) Run DQ report (append-only)
-:r "C:\DW\SQL_Warehouse_Project_Fortis_Aligned\sql\warehouse\08_dq_report_gold.sql"
+:r "C:\DW\SQL_Warehouse_Project\sql\warehouse\08_dq_report_gold.sql"
 
 -- =========================
 -- 2) Quick validation output
